2426c40bd3e512272970e35b9a41bca3af48d0c2
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-10-19 01:28:08 -0600

    Add Transforms

---
+++ src/SW-Tools-Net/Transforms/XslRetentionTransform.cs
@@ -0,0 +1,7 @@
+﻿namespace SW.Tools.Transforms;
+
+internal class XslRetentionTransform : XslBaseTransform
+{
+    internal XslRetentionTransform(string xml)
+        : base(xml, "retencion20.xslt") { }
+}
