2cf45a140d417f82f73a9e39562cffc0b754a184
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-10-19 01:29:17 -0600

    Add UT

---
+++ test/SW-Tools-Net-UnitTest/Helpers/CustomAssert.cs
@@ -0,0 +1,42 @@
+﻿using SW.Services.Stamp;
+using SW.Tools.Commons.Enum;
+using SW.Tools.Entities.Response;
+using Xunit.Sdk;
+
+namespace SW.Tools.UnitTest.Helpers;
+
+internal class CustomAssert
+{
+    internal static void ResultIsSuccess<T>(BaseResponse<T> result)
+    {
+        if (result == null || string.IsNullOrEmpty(result.Status) || result.Data == null) 
+            throw NotNullException.ForNullValue();
+        bool condition = result.Status.Equals(ResponseStatus.success.ToString());
+        if (!condition) 
+            throw TrueException.ForNonTrueValue($"Se esperaba Status igual a {ResponseStatus.success} pero fue {result.Status}", condition);
+    }
+    internal static void ResultIsError<T>(BaseResponse<T> result, string expectedMessage = null)
+    {
+        if (result == null || string.IsNullOrEmpty(result.Status) || string.IsNullOrEmpty(result.Message))
+            throw NotNullException.ForNullValue();
+        bool condition = result.Status.Equals(ResponseStatus.error.ToString());
+        if (!condition) 
+            throw TrueException.ForNonTrueValue($"Se esperaba {ResponseStatus.error} pero fue e {result.Status}", condition);
+        condition = !string.IsNullOrEmpty(result.Message);
+        if (!condition) 
+            throw TrueException.ForNonTrueValue($"Se esperaba que Message tuviera un valor pero fue {result.Message}", condition);
+        if (expectedMessage != null)
+        {
+            condition = result.Message.Equals(expectedMessage);
+            if (condition)
+                throw TrueException.ForNonTrueValue($"Se esperaba Message igual a {expectedMessage} pero fue {result.Message}", condition);
+        }
+    }
+    internal static void StampIsSuccess(Stamp stamp, string xml)
+    {
+        var result = stamp.TimbrarV1Async(xml).Result;
+        var condition = result != null && result.Status.Equals(ResponseStatus.success.ToString());
+        if (!condition)
+            throw TrueException.ForNonTrueValue("El timbrado no fue exitoso.", condition);
+    }
+}
