589fb4233d910d10cb73ef177afafc334184d7a6
Aeyrton Villalobos <aeyrton.villalobos@sw.com.mx>
2023-10-19 01:27:46 -0600

    Add XSLT Resources

---
+++ src/SW-Tools-Net/Resources/Resources.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace SW.Tools.Resources;
+
+internal class Resources
+{
+    private readonly List<string> _resources;
+    private readonly Assembly _assembly;
+    internal byte[] GetResource(string resourceName)
+    {
+        using var rs = _assembly.GetManifestResourceStream(_resources.FirstOrDefault(l => l.Contains(resourceName)));
+        using MemoryStream ms = new();
+        rs.CopyTo(ms);
+        return ms.ToArray();
+    }
+    internal List<string> GetAllResources() => _resources;
+    internal Assembly GetAssembly() => _assembly;
+    internal Resources()
+    {
+        _assembly = Assembly.GetAssembly(typeof(Resources));
+        _resources = _assembly.GetManifestResourceNames().ToList();
+    }
+}
