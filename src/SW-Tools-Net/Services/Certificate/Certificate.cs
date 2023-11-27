4225ffb160555f39b67a62225bc6da01569599f0
NanoCarrillo <fernando.carrillo@sw.com.mx>
2023-11-27 09:51:04 -0600

    Fix

--- src/SW-Tools-Net/Services/Certificate/Certificate.cs
+++ src/SW-Tools-Net/Services/Certificate/Certificate.cs
@@ -18,8 +18,8 @@ public class Certificate : CertificateService, ICertificate<CertificateResponse>
     /// Crea un PFX a partir de un archivo .Cer, .key y su respectiva contraseña.
     /// </summary>
     /// <returns>Un objeto CertificateResponse que contiene el PFX creado en formato de bytes.</returns>
-    public CertificateResponse CreatePfx(byte[] bytesCER, byte[] bytesKEY, string password)
+    public CertificateResponse CreatePfx(byte[] publicCertificate, byte[] privateKey, string password)
     {
-        return _handler.Execute(() => CertificatePfxService(bytesCER, bytesKEY, password));
+        return _handler.Execute(() => CertificatePfxService(publicCertificate, privateKey, password));
     }
 }
--- src/SW-Tools-Net/Services/Certificate/CertificateService.cs
+++ src/SW-Tools-Net/Services/Certificate/CertificateService.cs
@@ -1,17 +1,13 @@
 ﻿using SW.Tools.Helpers;
+using Chilkat;
 using SW.Tools.Validations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
-using Org.BouncyCastle.Crypto;
-using Org.BouncyCastle.Security;
-using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.X509;
+using SW.Tools.Validations;
 
 namespace SW.Tools.Services.Certificate;
 
@@ -19,21 +15,34 @@ public class CertificateService
 {
     protected internal CertificateService() { }
 
-    internal byte[] CertificatePfxService(byte[] bytesCER, byte[] bytesKEY, string password)
+    internal byte[] CertificatePfxService(byte[] publicCertificate, byte[] privateKey, string password)
     {
         try
         {
-            var certificate = new X509Certificate2(bytesCER, password);
+            ValidationCertificate.ValidateParams(publicCertificate, privateKey, password); 
 
-            char[] arrayOfChars = password.ToCharArray();
-            AsymmetricKeyParameter privateKey = PrivateKeyFactory.DecryptKey(arrayOfChars, bytesKEY);
-            RSACryptoServiceProvider subjectKey = CertificateUtils.ToRSA((RsaPrivateCrtKeyParameters)privateKey);
-            var PrivateKey = certificate.CopyWithPrivateKey(subjectKey);
-            return certificate.Export(X509ContentType.Pfx, password);
+            Cert cert = new();
+            if (!cert.LoadFromBinary(publicCertificate) || cert.CertVersion == 0)
+            {
+                throw new Exception("El certificado debe ser de extensión cer o crt.");
+            }
+            CertChain certChain = cert.GetCertChain();
+
+            var privKey = new PrivateKey();
+            if (!privKey.LoadPkcs8Encrypted(privateKey, password))
+            {
+                throw new Exception("La llave privada es incorrecta o la contraseña es invalida.");
+            }
+
+            Pfx pfx = new();
+            pfx.AddPrivateKey(privKey, certChain);
+            byte[] contenidoPfx = pfx.ToBinary(password);
+
+            return contenidoPfx;
         }
         catch (Exception ex)
         {
-            throw new Exception("Los datos del Certificado CER KEY o Password son incorrectos. No es posible leer la llave privada.", ex);
+            throw new Exception(ex.Message);
         }
     }
 }
